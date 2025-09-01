21be227026b3d7c4cc8aad0367fadd111b947092
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-09-01 14:24:23 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { PORT } = require("./server-config");
+const { PORT } = require("./config/server-config");
+const { sendBasicMail } = require("./services/email-services");
 
-const setupAndStartServer=()=>{
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
+const setupAndStartServer = () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(PORT,()=>{
-        console.log(`Server started at  ${PORT}`);
-        
-        
-    })
-}
+  app.listen(PORT, () => {
+    console.log(`Server started at  ${PORT}`);
+
+    sendBasicMail(
+      "support@admin.com",
+      "vikashg1596@gmail.com",
+      "This is a testing email",
+      "Hey,how are you, hope u like this"
+    );
+  });
+};
 setupAndStartServer();
---
+++ src/services/email-services.js
@@ -0,0 +1,26 @@
+const sender = require("../config/email-config");
+
+const sendBasicMail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+  try {
+    const response = await sender.sendMail({
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailBody,
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  sendBasicMail,
+};
+
+/**
+ * SMTP -> a@b.com
+ * receiver->d@e.com
+ *
+ * from:support@noti.com
+ */
