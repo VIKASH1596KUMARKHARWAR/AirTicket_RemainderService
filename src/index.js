7e646e1754451906afb454da4c04465715ebf5a5
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-09-02 08:49:13 +0530

    added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -2,27 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/server-config");
 
-// const { sendBasicMail } = require("./services/email-services");
 const jobs = require("./utils/job");
 
 const TicketController = require("./controllers/ticket-controller");
+const { createChannel } = require("./utils/messageQueues");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  // const channel = await createChannel();
 
   app.post("/api/v1/tickets", TicketController.create);
 
   app.listen(PORT, () => {
     console.log(`Server started at  ${PORT}`);
     jobs();
-    // sendBasicMail(
-    //   "support@admin.com",
-    //   "vikashg1596@gmail.com",
-    //   "This is a testing email",
-    //   "Hey,how are you, hope u like this"
-    //);
   });
 };
 setupAndStartServer();
